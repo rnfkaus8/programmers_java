0d4fc8a7a34c722edfe7cfe8d1402e586383c9db
rnfkaus8 <kth4350@gmail.com>
2021-12-28 00:00:06 +0900

    refact : int start, int end 변수 제거

--- app/src/main/java/taehyun/kim/solution/NumberOfK.java
+++ app/src/main/java/taehyun/kim/solution/NumberOfK.java
@@ -6,11 +6,7 @@ public class NumberOfK {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
     	for(int i = 0; i < commands.length; i++) {
-    		int start = 0;
-    		int end = 0;
-   			start = commands[i][0]-1;
-   			end = commands[i][1];
-            int[] tmp = Arrays.copyOfRange(array, start, end);
+            int[] tmp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
     		Arrays.sort(tmp);
     		answer[i] = tmp[commands[i][2]-1];
     	} 
